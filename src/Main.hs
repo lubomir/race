e182d0e105ab2c79df1bcb181725b4a759f77d11
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2016-01-23 15:14:33 +0100

    Tweak colors a bit

--- src/Main.hs
+++ src/Main.hs
@@ -37,7 +37,7 @@ colored :: Int -> B.ByteString -> B.ByteString
 colored i d = let col = colors !! i
               in "\ESC[" <> col <> "m" <> d <> "\ESC[0m\n"
   where
-    colors = cycle ["32", "33", "35", "36"]
+    colors = cycle ["34", "36", "35", "32", "33", "31"]
 
 reader :: Chan Msg -> Int -> IO ()
 reader _ 0 = return ()
